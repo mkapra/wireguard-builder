f460ef7ddf200b9753c4d5677f901b42319fe1aa
Maximilian Kapra <maximilian@kapra.de>
2021-09-30 21:58:52 +0200

    🗃️ Add initial migration

--- migrations/2021-09-26-194342_initial_setup/down.sql
+++ migrations/2021-09-26-194342_initial_setup/down.sql
@@ -1,1 +1,8 @@
--- This file should undo anything in `up.sql`
+DROP TABLE keys;
+DROP TABLE dns_servers;
+DROP TABLE allowed_ips;
+DROP TABLE vpn_nets;
+DROP TABLE vpn_ips;
+DROP TABLE clients;
+DROP TABLE clients;
+DROP TABLE client_allowed_ips;
