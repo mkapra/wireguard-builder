a2c3332f032e75354bbaa992ab40c3397fcf1eeb
Maximilian Kapra <maximilian@kapra.de>
2021-10-05 20:36:04 +0200

    ♻️ Move AppData to lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+use diesel::prelude::*;
+use diesel::sqlite::SqliteConnection;
+use dotenv::dotenv;
+use std::env;
+
+pub struct AppState {
+    pub db_connection: SqliteConnection,
+}
+
+pub fn establish_connection() -> SqliteConnection {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    SqliteConnection::establish(&database_url)
+        .expect(&format!("Error connecting to {}", database_url))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 use diesel::prelude::*;
-use diesel::sqlite::SqliteConnection;
-use dotenv::dotenv;
-use std::env;
 
 #[macro_use]
 extern crate diesel;
@@ -13,21 +10,9 @@ use models::Key;
 mod schema;
 use schema::keys::dsl::*;
 
-struct AppState {
-    db_connection: SqliteConnection,
-}
-
-pub fn establish_connection() -> SqliteConnection {
-    dotenv().ok();
-
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-
-    SqliteConnection::establish(&database_url)
-        .expect(&format!("Error connecting to {}", database_url))
-}
 
 #[post("/keys")]
-async fn post_key(data: web::Data<AppState>) -> impl Responder {
+async fn post_key(data: web::Data<wireguard_api_rs::AppState>) -> impl Responder {
     let key = Key::gen_key();
     let json_key = serde_json::to_string(&key).expect("Could not convert");
 
@@ -40,7 +25,7 @@ async fn post_key(data: web::Data<AppState>) -> impl Responder {
 }
 
 #[get("/keys")]
-async fn get_key(data: web::Data<AppState>) -> impl Responder {
+async fn get_key(data: web::Data<wireguard_api_rs::AppState>) -> impl Responder {
     let result = keys
         .load::<Key>(&data.db_connection)
         .expect("Could not get keys");
@@ -53,8 +38,8 @@ async fn get_key(data: web::Data<AppState>) -> impl Responder {
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .data(AppState {
-                db_connection: establish_connection(),
+            .data(wireguard_api_rs::AppState {
+                db_connection: wireguard_api_rs::establish_connection(),
             })
             .service(post_key)
             .service(get_key)
