3f9ce151c5c63126d72776e7c90f7b04d9a2fee3
Maximilian Kapra <maximilian@kapra.de>
2021-12-12 17:42:42 +0100

    Add vpn network resource

---
+++ server/validator.js
@@ -0,0 +1,5 @@
+// Validates the given ip address
+module.exports.isIP = function(ip) {
+    var ipRegex = /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/;
+    return ipRegex.test(ip);
+};
