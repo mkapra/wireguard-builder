d635417759b9f5ae5626433449837f7c207e2aa1
Maximilian Kapra <maximilian@kapra.de>
2021-12-26 20:27:34 +0100

    [Client] Add react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,20 @@
+import { Outlet, Link } from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
       <h1>Wireguard Builder</h1>
+
+      <nav style={{
+        borderBottom: "solid 1px",
+        paddingBottom: "1rem"
+        }}
+      >
+        <Link to="/">Home</Link> | { " " }
+        <Link to="/about">About</Link>
+      </nav>
+
+      <Outlet />
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
+import About from './About';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} >
+          <Route path="/about" element={<About />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
