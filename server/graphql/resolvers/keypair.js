34d24384d5de21738199782fe0dda6d060f54c52
Maximilian Kapra <maximilian@kapra.de>
2021-12-27 16:33:41 +0100

    ♻️ Refactor Keypairs

--- server/graphql/resolvers/keypair.js
+++ server/graphql/resolvers/keypair.js
@@ -1,34 +1,32 @@
 const { ApolloError } = require("apollo-server");
 
 module.exports = {
-    // Resolver for the keypair.
-    Query: {
-        keypairs: async(_, __, { dataSources: { db } }) => {
-            return db.getKeypairs();
-        },
-        keypair: async(_, { id }, { dataSources: { db } }) => {
-            return db.getKeypairById(id).then(keypair => {
-                return keypair;
-            });
-        }
+  // Resolver for the keypair.
+  Query: {
+    keypairs: async (_, __, { dataSources }) => {
+      return dataSources.db.getKeypairs();
     },
-    Mutation: {
-        generateKeypair: async(_, __, { dataSources: { db } }) => {
-            const keypair = await db.createKeypair();
-            return keypair;
-        },
-        deleteKeypair: async(_, { id }, { dataSources: { db } }) => {
-            return db.deleteKeypair(id)
-                .then((count) => {
-                    if (count === 1) {
-                        return `Keypair with id ${id} deleted!`;
-                    } else {
-                        throw new ApolloError(`Keypair with id ${id} not found!`);
-                    }
-                })
-                .catch((err) => {
-                    throw new ApolloError(err);
-                });
-        }
-    }
+    keypair: async (_, { id }, { dataSources }) => {
+      return dataSources.db.getKeypairById(id);
+    },
+  },
+  Mutation: {
+    generateKeypair: async (_, __, { dataSources }) => {
+      return dataSources.db.createKeypair();
+    },
+    deleteKeypair: async (_, { id }, { dataSources }) => {
+      return dataSources.db
+        .deleteKeypair(id)
+        .then((count) => {
+          if (count === 1) {
+            return `Keypair with id '${id}' deleted!`;
+          } else {
+            throw new ApolloError(`Keypair with id ${id} not found!`);
+          }
+        })
+        .catch((err) => {
+          throw new ApolloError(err);
+        });
+    },
+  },
 };
