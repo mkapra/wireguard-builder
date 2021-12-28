b425d11cf753e1e8449e365c4ef7312984f1ede1
Maximilian Kapra <maximilian@kapra.de>
2021-12-28 19:49:41 +0100

    ✨ [Client] Add generation for keypairs

--- client/src/KeypairList.js
+++ client/src/KeypairList.js
@@ -1,5 +1,5 @@
-import { useQuery, gql } from "@apollo/client";
-import { useEffect } from "react";
+import { useQuery, useMutation, gql } from "@apollo/client";
+import { toast } from "react-toastify";
 
 import Table from "./Table";
 
@@ -12,37 +12,72 @@ const GET_KEYPAIRS = gql`
   }
 `;
 
-const KeypairList = () => {
-  const { loading, error, data } = useQuery(GET_KEYPAIRS);
+const GENERATE_KEYPAIR = gql`
+  mutation Mutation {
+    generateKeypair {
+      id
+    }
+  }
+`;
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
+const KeypairList = () => {
+  const {
+    loading: listLoading,
+    error: listError,
+    data: listData,
+  } = useQuery(GET_KEYPAIRS);
+  const [
+    generateKeypair,
+    {
+      data: generateData,
+      loading: generateLoading,
+      error: generateError,
+      reset: generateReset,
+    },
+  ] = useMutation(GENERATE_KEYPAIR, {
+    refetchQueries: [{ query: GET_KEYPAIRS }],
+  });
+
+  if (listLoading) return <p>Loading...</p>;
+  if (listError) return <p>Error :(</p>;
+  if (generateLoading) return <p>Generate Keypair...</p>;
+  if (generateError) return <p>Error :(</p>;
+
+  const successGeneratedKeypair = (id) => {
+    toast.success(`Keypair with id ${id} created successfully`, { toastId: id });
+  };
 
   return (
     <div>
+      {generateData &&
+        generateReset &&
+        successGeneratedKeypair(generateData.generateKeypair.id)}
+
       <h2 className="text-3xl mb-4">Keypairs</h2>
 
-      <button className="mb-4 float-right flex space-x-2 bg-blue-500 items-center justify-center py-2 px-4 rounded-full text-blue-100 hover:bg-orange-400 hover:text-gray-800 transition delay-75">
+      <button
+        onClick={() => generateKeypair()}
+        className="mb-4 float-right flex space-x-2 bg-blue-500 items-center justify-center py-2 px-4 rounded-full text-blue-100 hover:bg-orange-400 hover:text-gray-800 transition delay-75"
+      >
         <span>Generate Keypair</span>
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          class="h-6 w-6"
+          className="h-6 w-6"
           fill="none"
           viewBox="0 0 24 24"
           stroke="currentColor"
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
             d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
           />
         </svg>
       </button>
-
       <Table
         headings={["ID", "Public Key"]}
-        data={data.keypairs}
+        data={listData.keypairs}
         actions={true}
       />
     </div>
--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -29,15 +29,15 @@ const Navbar = () => {
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            class="h-6 w-6"
+            className="h-6 w-6"
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
               d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
             />
           </svg>
@@ -49,15 +49,15 @@ const Navbar = () => {
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            class="h-6 w-6"
+            className="h-6 w-6"
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
               d="M15 7a2 2 0 012 2m4 0a6 6 0 01-7.743 5.743L11 17H9v2H7v2H4a1 1 0 01-1-1v-2.586a1 1 0 01.293-.707l5.964-5.964A6 6 0 1121 9z"
             />
           </svg>
@@ -68,16 +68,16 @@ const Navbar = () => {
           className="flex space-x-2 py-2 px-4 transition delay-75 hover:bg-orange-400 hover:text-blue-800 rounded"
         >
           <svg
-            class="w-6 h-6"
+            className="w-6 h-6"
             fill="none"
             stroke="currentColor"
             viewBox="0 0 24 24"
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
               d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"
             ></path>
           </svg>
@@ -89,15 +89,15 @@ const Navbar = () => {
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            class="h-6 w-6"
+            className="h-6 w-6"
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
               d="M3 15a4 4 0 004 4h9a5 5 0 10-.1-9.999 5.002 5.002 0 10-9.78 2.096A4.001 4.001 0 003 15z"
             />
           </svg>
@@ -108,16 +108,16 @@ const Navbar = () => {
           className="flex space-x-2 py-2 px-4 transition delay-75 hover:bg-orange-400 hover:text-blue-800 rounded"
         >
           <svg
-            class="w-6 h-6"
+            className="w-6 h-6"
             fill="none"
             stroke="currentColor"
             viewBox="0 0 24 24"
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
               d="M12 18h.01M8 21h8a2 2 0 002-2V5a2 2 0 00-2-2H8a2 2 0 00-2 2v14a2 2 0 002 2z"
             ></path>
           </svg>
@@ -128,16 +128,16 @@ const Navbar = () => {
           className="flex space-x-2 py-2 px-4 transition delay-75 hover:bg-orange-400 hover:text-blue-800 rounded"
         >
           <svg
-            class="w-6 h-6"
+            className="w-6 h-6"
             fill="none"
             stroke="currentColor"
             viewBox="0 0 24 24"
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
               d="M5 12h14M5 12a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v4a2 2 0 01-2 2M5 12a2 2 0 00-2 2v4a2 2 0 002 2h14a2 2 0 002-2v-4a2 2 0 00-2-2m-2-4h.01M17 16h.01"
             ></path>
           </svg>
