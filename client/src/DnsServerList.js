faece1ef67d21a335bbc2807d42e92193f2b31d0
Maximilian Kapra <maximilian@kapra.de>
2021-12-29 13:38:59 +0100

    ♻️ [Client] Add error component for all fatal errors

--- client/src/DnsServerList.js
+++ client/src/DnsServerList.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 import Table from "./Table";
 import Searchbar from "./Searchbar";
 import NewDnsServer from "./NewDnsServer";
+import Error from "./Error";
 
 const GET_DNS_SERVERS = gql`
   query Query {
@@ -22,53 +23,59 @@ const DnsServerList = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
 
   return (
-    <div>
+    <>
       <h2 className="text-3xl mb-4">DNS Servers</h2>
+      {error && <Error error={error} />}
 
-      <button
-        onClick={() => setIsOpen(true)}
-        className="mb-4 float-right flex space-x-2 bg-blue-500 items-center justify-center py-2 px-4 rounded-full text-blue-100 hover:bg-orange-400 hover:text-gray-800 transition delay-75"
-      >
-        <span>New DNS Server</span>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-          />
-        </svg>
-      </button>
+      {!error && (
+        <>
+          <button
+            onClick={() => setIsOpen(true)}
+            className="mb-4 float-right flex space-x-2 bg-blue-500 items-center justify-center py-2 px-4 rounded-full text-blue-100 hover:bg-orange-400 hover:text-gray-800 transition delay-75"
+          >
+            <span>New DNS Server</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+          </button>
 
-      <div className="space-y-2">
-        <Searchbar
-          search={search}
-          setSearch={setSearch}
-          placeholder="Search for name or ip address..."
-        />
+          <div className="space-y-2">
+            <Searchbar
+              search={search}
+              setSearch={setSearch}
+              placeholder="Search for name or ip address..."
+            />
 
-        <Table
-          headings={["ID", "Name", "Description", "IP Address"]}
-          data={data.dnsServers.filter((dnsServer) => {
-            return (
-              dnsServer.name.toLowerCase().includes(search.toLowerCase()) ||
-              dnsServer.ip_address.toLowerCase().includes(search.toLowerCase())
-            );
-          })}
-        />
-      </div>
+            <Table
+              headings={["ID", "Name", "Description", "IP Address"]}
+              data={data.dnsServers.filter((dnsServer) => {
+                return (
+                  dnsServer.name.toLowerCase().includes(search.toLowerCase()) ||
+                  dnsServer.ip_address
+                    .toLowerCase()
+                    .includes(search.toLowerCase())
+                );
+              })}
+            />
+          </div>
 
-      {isOpen && <NewDnsServer setIsOpen={setIsOpen} />}
-    </div>
+          {isOpen && <NewDnsServer setIsOpen={setIsOpen} />}
+        </>
+      )}
+    </>
   );
 };
 
--- client/src/KeypairList.js
+++ client/src/KeypairList.js
@@ -2,6 +2,7 @@ import { useQuery, useMutation, gql } from "@apollo/client";
 import { toast } from "react-toastify";
 
 import Table from "./Table";
+import Error from "./Error";
 
 const GET_KEYPAIRS = gql`
   query Query {
@@ -49,9 +50,7 @@ const KeypairList = () => {
   });
 
   if (listLoading) return <p>Loading...</p>;
-  if (listError) return <p>Error :(</p>;
   if (generateLoading) return <p>Generate Keypair...</p>;
-  if (generateError) return <p>Error :(</p>;
 
   const successGeneratedKeypair = (id) => {
     toast.success(`Keypair with id ${id} created successfully`, {
@@ -76,39 +75,44 @@ const KeypairList = () => {
   };
 
   return (
-    <div>
-      {generateData &&
-        generateReset &&
-        successGeneratedKeypair(generateData.generateKeypair.id)}
-
+    <>
       <h2 className="text-3xl mb-4">Keypairs</h2>
+      {listError && <Error error={listError} />}
+      {generateError && <Error error={generateError} />}
+      {!listError && !generateError && (
+        <div>
+          {generateData &&
+            generateReset &&
+            successGeneratedKeypair(generateData.generateKeypair.id)}
 
-      <button
-        onClick={() => generateKeypair()}
-        className="mb-4 float-right flex space-x-2 bg-blue-500 items-center justify-center py-2 px-4 rounded-full text-blue-100 hover:bg-orange-400 hover:text-gray-800 transition delay-75"
-      >
-        <span>Generate Keypair</span>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+          <button
+            onClick={() => generateKeypair()}
+            className="mb-4 float-right flex space-x-2 bg-blue-500 items-center justify-center py-2 px-4 rounded-full text-blue-100 hover:bg-orange-400 hover:text-gray-800 transition delay-75"
+          >
+            <span>Generate Keypair</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+          </button>
+          <Table
+            headings={["ID", "Public Key"]}
+            data={listData.keypairs}
+            onDelete={handleDeleteKeypair}
           />
-        </svg>
-      </button>
-      <Table
-        headings={["ID", "Public Key"]}
-        data={listData.keypairs}
-        onDelete={handleDeleteKeypair}
-      />
-    </div>
+        </div>
+      )}
+    </>
   );
 };
 
