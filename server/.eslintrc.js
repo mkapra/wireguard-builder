8a7dbf30473d677e41a80f87e46358bff0b76f87
Maximilian Kapra <maximilian@kapra.de>
2021-12-31 20:05:49 +0100

    Recreate eslintrc

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,22 +1,13 @@
 module.exports = {
     "env": {
-        "browser": true,
-        "es2021": true
+        "commonjs": true,
+        "es2021": true,
+        "node": true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
+    "extends": "eslint:recommended",
     "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 13,
-        "sourceType": "module"
+        "ecmaVersion": 13
     },
-    "plugins": [
-        "react"
-    ],
     "rules": {
     }
 };
