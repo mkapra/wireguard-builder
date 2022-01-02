5d44c01e2f7f8ee27d884754b8f8bb5f49327ac8
Maximilian Kapra <maximilian@kapra.de>
2022-01-03 00:15:23 +0100

    ♻️ [Client] Refactor navbar
    Move Navbar to App.js and refactor the link appearance

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import { Outlet } from "react-router-dom";
 
+import Navbar from "./Navbar";
+
 function App() {
   return (
-    <div className="App p-10 flex-1 overflow-y-auto">
-      <Outlet />
+    <div className="App flex overflow-hidden h-screen">
+      <Navbar />
+      <div className="p-10 flex-1 overflow-y-auto">
+        <Outlet />
+      </div>
     </div>
   );
 }
