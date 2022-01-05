8ae1ce96643c89dd6ed6bf4b17af19766cca2115
Maximilian Kapra <maximilian+github@kapra.de>
2022-01-05 20:31:04 +0100

    ✨ Vpn network modal (#61)
    * ✨ Add modal for creating vpn networks
    
    * 🐛  Change name of variables for success of mutation query
    
    * 🎨  Run prettier on VpnNetworkList

--- client/src/VpnNetworkList.js
+++ client/src/VpnNetworkList.js
@@ -4,6 +4,8 @@ import { useQuery, gql } from "@apollo/client";
 import Table from "./Table";
 import Searchbar from "./Searchbar";
 import Error from "./Error";
+import NewVpnNetwork from "./NewVpnNetwork";
+import Button from "./Button";
 
 const GET_VPN_NETWORKS = gql`
   query Query {
@@ -22,6 +24,7 @@ const GET_VPN_NETWORKS = gql`
 const VpnNetworkList = () => {
   const { loading, error, data } = useQuery(GET_VPN_NETWORKS);
   const [search, setSearch] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
 
   if (loading) return <p>Loading...</p>;
 
@@ -40,26 +43,53 @@ const VpnNetworkList = () => {
       {error && <Error error={error} />}
 
       {!error && (
-        <div className="space-y-2">
-          <Searchbar search={search} setSearch={setSearch} />
-          <Table
-            headings={[
-              "ID",
-              "Name",
-              "Description",
-              "IP-Address",
-              "Subnetmask",
-              "Port",
-              "Interface",
-            ]}
-            data={data.vpnNetworks.filter((network) =>
-              matches(network, search)
-            )}
-          />
-        </div>
+        <>
+          <div className="float-right mb-4">
+            <Button onClick={() => setIsOpen(true)}>
+              <div className="flex space-x-2">
+                <span>New VPN Network</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+              </div>
+            </Button>
+          </div>
+
+          <div className="space-y-2">
+            <Searchbar search={search} setSearch={setSearch} />
+            <Table
+              headings={[
+                "ID",
+                "Name",
+                "Description",
+                "IP-Address",
+                "Subnetmask",
+                "Port",
+                "Interface",
+              ]}
+              data={data.vpnNetworks.filter((network) =>
+                matches(network, search)
+              )}
+            />
+          </div>
+
+          {isOpen && <NewVpnNetwork setIsOpen={setIsOpen} />}
+        </>
       )}
     </>
   );
 };
 
 export default VpnNetworkList;
+export { GET_VPN_NETWORKS };
